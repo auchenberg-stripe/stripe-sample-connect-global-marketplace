ab1d26e96b2b78c7cf9276b7c11dcad9a6498f47
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-05 20:43:16 -0700

    Manual merge of PR #5

--- client/helpers/stripe.js
+++ client/helpers/stripe.js
@@ -1,7 +1,7 @@
 let config = {
   stripe: {
     publicKey: 'pk_test_nuvZwgc7ySc2JyhFMdirs5QS00pVixsTUL',
-    secretKey: 'sk_test_XLsq1pM0yZB7i959v1Ki7Xi700bFGWcg1n',
+    secretKey: 'sk_test_Idi0Pelyc4muzXky3O3w2UT100mcj9wd2v',
     clientId: 'ca_Fi3XMTp6tQ0sbM0AEdxzUv08SzYYGS5x',
   },
 };
