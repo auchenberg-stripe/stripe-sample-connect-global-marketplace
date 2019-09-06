257c7ed4c8ec596e7f8b05de8fb266f3803831ad
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-05 21:09:20 -0700

    Remove hard coded urls

--- client/helpers/stripe.js
+++ client/helpers/stripe.js
@@ -1,7 +1,7 @@
 let config = {
   stripe: {
-    publicKey: 'pk_test_nuvZwgc7ySc2JyhFMdirs5QS00pVixsTUL',
-    secretKey: 'sk_test_Idi0Pelyc4muzXky3O3w2UT100mcj9wd2v',
+    publicKey: 'pk_live_DoLLbRZ5CwP4ExwB2U1G4oBR006tPvhliP',
+    secretKey: 'sk_live_G76rTKPqh0vgncC3Hpsbj8Yo00O1BKnDjo',
     clientId: 'ca_Fi3XMTp6tQ0sbM0AEdxzUv08SzYYGS5x',
   },
 };
