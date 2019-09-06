38e022769552fd9e279fbdd7cd7f1d527c413791
Kenneth Auchenberg <auchenberg@stripe.com>
2019-09-06 13:17:39 -0700

    Always use TEST keys for Gelato test mode

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,8 +1,15 @@
 let config = {
   stripe: {
-    publicKey: 'pk_live_DoLLbRZ5CwP4ExwB2U1G4oBR006tPvhliP',
-    secretKey: 'sk_live_G76rTKPqh0vgncC3Hpsbj8Yo00O1BKnDjo',
-    clientId: 'ca_Fi3XMTp6tQ0sbM0AEdxzUv08SzYYGS5x',
+    live: {
+      publicKey: 'pk_live_DoLLbRZ5CwP4ExwB2U1G4oBR006tPvhliP',
+      secretKey: 'sk_live_G76rTKPqh0vgncC3Hpsbj8Yo00O1BKnDjo',
+      clientId: 'ca_FkyHxhkunnYrn0giRjS6lAffLFSLj2x7',
+    },
+    test: {
+      publicKey: 'pk_test_lJRRI4dl2VQ4tFp3p704M0uA0007b4kfq4',
+      secretKey: 'sk_test_Y7mFZRt3zztcxVRD7brzYrwh00MewcLyTm',
+      clientId: 'ca_FkyHCg7X8mlvCUdMDao4mMxagUfhIwXb',
+    },
   },
 };
 
