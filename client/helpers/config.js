b7ab49462e97ec2a04792676cbba1e80c0b33c84
Kenneth Auchenberg <auchenberg@stripe.com>
2019-11-26 16:27:36 -0800

    Update Stripe keys

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,14 +1,14 @@
 let config = {
   stripe: {
     live: {
-      publicKey: 'pk_live_DoLLbRZ5CwP4ExwB2U1G4oBR006tPvhliP',
-      secretKey: 'sk_live_G76rTKPqh0vgncC3Hpsbj8Yo00O1BKnDjo',
-      clientId: 'ca_FkyHxhkunnYrn0giRjS6lAffLFSLj2x7',
+      publicKey: 'pk_live_jgxo342tQQEvOrpN4l4X2MRV00XHsEV7m2',
+      secretKey: 'sk_live_yTeqxBw6ddapXHc5YVZAnKo000HC42qWFF',
+      clientId: 'ca_GFjJk7F3RPqdQwaGFp3Hk0G7l1EZCyHU',
     },
     test: {
-      publicKey: 'pk_test_lJRRI4dl2VQ4tFp3p704M0uA0007b4kfq4',
-      secretKey: 'sk_test_Y7mFZRt3zztcxVRD7brzYrwh00MewcLyTm',
-      clientId: 'ca_FkyHCg7X8mlvCUdMDao4mMxagUfhIwXb',
+      publicKey: 'pk_test_TCUZLjVB1S5Ffq2GBacfHSnP00BZwvUilj',
+      secretKey: 'sk_test_IEmPp3szfBxKY5HCU0M08vOE00AJeUeR03',
+      clientId: 'ca_GFjJh4sAQYnMud62OsOPsxmcqALJB2wO',
     },
   },
 };
